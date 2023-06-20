7359e4557f8e159b9d1baece9b03e1892194a0d1
iblutrifork <iblu@trifork.com>
2023-06-20 12:59:11 +0200

    Remove top level await

--- action.js
+++ action.js
@@ -3,13 +3,17 @@ const github = require( '@actions/github' );
 const {generatePng} = require( './crawler' );
 const spawn = require( 'child_process' ).spawn;
 
-try {
+const run = async function() {
     const dockerComposePath = core.getInput( 'docker-compose-directory' );
     const configFile = core.getInput( 'input-file' );
     const outputFile = core.getInput( 'output-file' );
-    spawn( 'node', ['node_modules/puppeteer/install.js'] );
-    generatePng( dockerComposePath, configFile, outputFile );
+    // spawn( 'node', ['node_modules/puppeteer/install.js'] );
+    await generatePng( dockerComposePath, configFile, outputFile );
     console.log( 'Success' );
+}
+
+try {
+    run();
 } catch ( error ) {
     core.setFailed( error.message );
 }
