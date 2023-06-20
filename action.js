958a8b877f902b33f7eb15ed749e7610477d9a6a
iblutrifork <iblu@trifork.com>
2023-06-20 12:43:39 +0200

    Run puppeteer install before runnin script

--- action.js
+++ action.js
@@ -1,15 +1,15 @@
 const core = require( '@actions/core' );
 const github = require( '@actions/github' );
 const {generatePng} = require( './crawler' );
-const spawn = require('child_process').spawn;
+const spawn = require( 'child_process' ).spawn;
 
 try {
-    const dockerComposePath = core.getInput('docker-compose-directory');
-    const configFile = core.getInput('input-file');
-    const outputFile = core.getInput('output-file');
-    spawn('yarn', ['install']);
-    generatePng(dockerComposePath, configFile, outputFile);
-    console.log('Success');
+    const dockerComposePath = core.getInput( 'docker-compose-directory' );
+    const configFile = core.getInput( 'input-file' );
+    const outputFile = core.getInput( 'output-file' );
+    spawn( 'node', ['node_modules/puppeteer/install.js'] );
+    generatePng( dockerComposePath, configFile, outputFile );
+    console.log( 'Success' );
 } catch ( error ) {
     core.setFailed( error.message );
 }
