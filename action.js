542331733416d8ae7277df743694f443f1b9937b
iblutrifork <iblu@trifork.com>
2023-06-20 12:40:30 +0200

    Run yarn install before running action

--- action.js
+++ action.js
@@ -1,11 +1,13 @@
 const core = require( '@actions/core' );
 const github = require( '@actions/github' );
 const {generatePng} = require( './crawler' );
+const spawn = require('child_process').spawn;
 
 try {
     const dockerComposePath = core.getInput('docker-compose-directory');
     const configFile = core.getInput('input-file');
     const outputFile = core.getInput('output-file');
+    spawn('yarn', ['install']);
     generatePng(dockerComposePath, configFile, outputFile);
     console.log('Success');
 } catch ( error ) {
